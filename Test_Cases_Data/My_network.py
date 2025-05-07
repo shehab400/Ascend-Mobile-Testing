289447a865cace143172ab647093866768a043ef
shehap <115353206+shehab400@users.noreply.github.com>
2025-05-08 02:23:19 +0300

    Remove test cases data, added gitignore

--- Test_Cases_Data/My_network.py
+++
@@ -1,6 +0,0 @@
-class MyNetworkTestCases:
-    FULL_NAME = 'Shehap Elhadary'
-    COMPANY_NAME = 'Microsoft'
-    INDUSTRY_NAME = 'Software engineer'
-    FRIEND_NAME = 'Shehap Elhadary'
-    PUBLIC_PROFILE_NAME = 'David j. malan'
